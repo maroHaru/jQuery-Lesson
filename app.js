060173408b4fbe61571eedd4c695c482d20ae598
maroHaru <134841618+maroHaru@users.noreply.github.com>
2024-02-24 04:26:44 +0000

    [Add] Manipulate HTML CSS elements with jQuery

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').slideDown(1000);
+// });
+
+// $(function(){
+//   $('.box1').slideUp(1000);
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').hide(1000);
+// });
+
+$(function(){
+  $('.box1').slideDown(1000, function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+  }).slideUp(1000);
+  });
 });
