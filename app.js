1eb2a00eb2c93c59997dd401a79718203b53f013
maroHaru <134841618+maroHaru@users.noreply.github.com>
2024-02-27 12:18:08 +0000

    [Add] Implement mouse operation event

--- app.js
+++ app.js
@@ -26,12 +26,18 @@
 //   $('.box1').hide(1000);
 // });
 
+// $(function(){
+//   $('.box1').slideDown(1000, function (){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'height': '100px',
+//       'width': '200px'
+//   }).slideUp(1000);
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(1000, function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '100px',
-      'width': '200px'
-  }).slideUp(1000);
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
   });
 });
